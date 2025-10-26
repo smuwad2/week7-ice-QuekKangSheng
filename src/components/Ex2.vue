7d0f06bf32510a3c5133fcd2d94af2046523ce67
Quek Kang Sheng <ks.quek.2023@smu.edu.sg>
2025-10-26 11:48:43 +0000

    done Ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,18 +1,27 @@
 <script>
-    export default { 
-        // Add code in appropriate places
-        data() {
-            return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
-            }
-            
+export default { 
+    data() {
+        return {
+            items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+            newItem: ""
+        }
+    },
+    methods: {
+        addItem() {
+            this.items.push(this.newItem)
+            this.newItem = "" // optional: clears the input after adding
         }
     }
+}
 </script>
 
 <template>
-
     <h2>Shopping Cart</h2>
-    <!-- TODO: Add Code Here -->
-   
+    <ul>
+        <li v-for="(item, idx) in items">
+            {{ item }} <button @click="items.splice(idx, 1)">Delete!</button>
+        </li>
+    </ul>
+    <input type="text" v-model="newItem"/>
+    <button @click="addItem">Add!</button>
 </template>
